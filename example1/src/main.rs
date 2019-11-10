3c1e0e53f08518926bbf985d5a1a03b26a8e651e
lbali <juan.lucas.bali@gmail.com>
2019-11-10 00:03:09 -0300

    Un ejemplo terminado de adivinanzas

--- example1/src/main.rs
+++ example1/src/main.rs
@@ -7,14 +7,26 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
     println!("Input your guess:");
+    let mut attempts = 0;
     let secret_number = rand::thread_rng().gen_range(1,101);
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).
-        expect("Failed to read line");
-    println!("You guessed: {}", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println("Too small!"),
-        Ordering::Greater => println("Too big!"),
-        Ordering::Equal => println("Correct!"),
+    loop{
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).
+            expect("Failed to read line");
+        attempts = attempts + 1;
+        //let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Correct after {} attempts!", attempts);
+                break;
+            },
+        }
     }
 }
